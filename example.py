3ad06a5fe630b745f4ad1fed1dfef85e8be3045d
czmorekakos <czmorekakos@gmail.com>
2023-12-14 13:32:01 +0100

    Done

--- example.py
+++ example.py
@@ -1,8 +1,15 @@
 import random as rr
 
-def isItPerfect():
-    pass
-
+def isItPerfect(n):
+    if n == 1 or n == 0:
+        return False
+    else:  
+        divNumber = [1]
+        for i in range(2, n, 1):
+            if n % i == 0:
+                divNumber.append(i)
+        return sum(divNumber) == n
+            
 def randomGenerator(s, e, a):
     numbers = []
     for i in range(a):
@@ -18,6 +25,9 @@ def makeNumber(text):
         except ValueError:
             print("Helytelen érték!")
 
+perfectNumbers = []            
+pnf = dict()
+
 startMessage = "kezdőérték: "
 endMessage = "Végérték: "
 amountMessage = "Értékek száma: "
@@ -26,4 +36,18 @@ start = makeNumber(startMessage)
 end = makeNumber(endMessage)
 amount = makeNumber(amountMessage)
 
-randomGenerator(start, end, amount)
+randomNumber = randomGenerator(start, end, amount)
+
+for num in randomNumber:
+    if isItPerfect(num):
+        perfectNumbers.append(num)
+
+
+for num in perfectNumbers:
+    if num in pnf.keys():
+        pnf[num] += 1
+    else:
+        pnf[num] = 1
+        
+for key in pnf.keys():
+    print(f"{key}: {pnf[key]} db")
